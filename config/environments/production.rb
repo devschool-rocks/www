42250471c8e4ad630b9f3337d895c1d75c3bf2b6
Jim OKelly <thatrubylove@gmail.com>
2015-12-13 16:23:18 -0600

    moved assets to url vs path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # leverage browser caching
+  config.static_cache_control = "public, max-age=31536000"
+  # utilize a CDN to deliver static assets
   config.action_controller.asset_host = "d3ura4x2gavgml.cloudfront.net"
 
   # Settings specified here will take precedence over those in config/application.rb.
@@ -24,7 +27,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
