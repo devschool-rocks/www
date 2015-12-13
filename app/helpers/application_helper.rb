d6eef8f0caf677d8a0a97b46d3280c890c534f04
Jim OKelly <thatrubylove@gmail.com>
2015-12-13 17:21:50 -0600

    restructured meta titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def meta_title
+    custom = content_for(:title)
+    default = "Become a Web Developer in about 420 hour of guided instruction by an industry expert programmer!"
+    "Devschool: #{custom || default}"
+  end
 end
