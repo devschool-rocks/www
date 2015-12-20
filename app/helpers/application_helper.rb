34305786094f8eced02adba8c7b4f0e2b6c9400f
Jim OKelly <thatrubylove@gmail.com>
2015-12-19 23:21:07 -0600

    only one description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,15 @@ module ApplicationHelper
     "Devschool: #{custom || default}"
   end
 
+  def meta_description
+    "An online based web development bootcamp/school consisting of 40 private one on one expert sessions and 420 hours of group and self study."
+  end
+
+  def meta_keywords
+    "devschool, devschool.rocks, ruby, rails, html, css, javascript, bootcamp"
+  end
+
+
   def backend_url
     if Rails.env.production?
       "https://devschool.rocks"
