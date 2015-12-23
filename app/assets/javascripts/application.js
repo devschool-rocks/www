aa0267142229d963ca1f4b159f4cf11be5d05847
Jim OKelly <thatrubylove@gmail.com>
2015-12-23 14:35:46 -0600

    removed nofol on socials and compressed some css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,27 +14,7 @@
 //= require bootstrap.min
 //= require smooth-scroll.min
 //= require slick.min
-//= require clickdesk
+//= require clickdesk.min
 
 //= require_tree .
-//= require_self
 
-smoothScroll.init();
-
-$(document).ready(function() {
-
-  $('.slide').slick({
-    autoplay: true,
-    accessibility: true,
-    draggable: true,
-    autoplaySpeed: 10000
-  });
-
-  $('.navbar-nav a[href="' + this.location.pathname + '"]')
-    .addClass('active');
-
-  if( $('.navbar-nav a.active').length == 0 ) {
-    $(".navbar-nav a.default-active")
-      .addClass('active');
-  }
-});
