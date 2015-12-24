c2076df7b4cfe62861ef833f9d338a97c848c6ab
Jim OKelly <thatrubylove@gmail.com>
2015-12-24 09:32:42 -0600

    fixed duplicate what home issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  match "what" => "web_page#what",   via: :get,   as: :what
+  match "/" =>    "web_page#what",   via: :get,   as: :what
   match "how"  => "web_page#how",    via: :get,   as: :how
   match "where"=> "web_page#where",  via: :get,   as: :where
   match "who"  => "web_page#who",    via: :get,   as: :who
   match "price"=> "web_page#price",  via: :get,   as: :price
   match "terms"=> "web_page#terms",  via: :get,   as: :terms
-
 
   match "apply"  => "web_page#apply",  via: :get,   as: :apply
   match "applied"=> "web_page#applied",via: :get,   as: :applied
