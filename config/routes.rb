c49a84edb7113c56ec66bdba2918637dadd33788
Jim OKelly <thatrubylove@gmail.com>
2015-12-25 02:12:17 -0600

    added ngo page

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   match "where"=> "web_page#where",  via: :get,   as: :where
   match "who"  => "web_page#who",    via: :get,   as: :who
   match "price"=> "web_page#price",  via: :get,   as: :price
+
   match "terms"=> "web_page#terms",  via: :get,   as: :terms
 
   match "apply"  => "web_page#apply",  via: :get,   as: :apply
   match "applied"=> "web_page#applied",via: :get,   as: :applied
+
+  match "ngo-program"=> "web_page#ngo",  via: :get,   as: :ngo
 
   get "/sitemap", controller: :sitemap, action: :index
 
