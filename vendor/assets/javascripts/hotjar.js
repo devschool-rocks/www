8ddd80ef5b08acf23db6e680c4f958de116d09ea
Jim OKelly <thatrubylove@gmail.com>
2015-12-26 02:18:57 -0600

    cleaned assets

--- vendor/assets/javascripts/hotjar.js
+++ vendor/assets/javascripts/hotjar.js
@@ -1,5 +1,3 @@
-<!-- Hotjar Tracking Code for https://devschool.rocks -->
-<script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:126025,hjsv:5};
@@ -8,5 +6,4 @@
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
 
