e39cce317286fc2cf3435123e78601794365f983
Jim OKelly <thatrubylove@gmail.com>
2015-12-26 02:08:20 -0600

    no more 500s in blog

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -6,4 +6,17 @@ class BlogController < ApplicationController
     @articles = ARTICLES
   end
 
+  def show
+    articles = Dir["#{Rails.root}/app/views/blog/_*"].
+      map {|f| f.split("/")[-1].gsub(".html.erb","") }.
+      map {|f| f[1..-1] }
+
+    if articles.grep(/params[:id]/).any?
+      @partial = params[:id].gsub("-","_")
+    else
+      @articles = ARTICLES
+      render :index and return
+    end
+  end
+
 end
