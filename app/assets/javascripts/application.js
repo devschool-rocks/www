3509764ca6a74f5753d74d66ece7000a19e82e2b
Jim OKelly <thatrubylove@gmail.com>
2015-12-28 14:19:20 -0600

    fixed menu error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,26 @@
 
 //= require_tree .
 
+smoothScroll.init();
+
+$(document).ready(function() {
+
+  $('.slide').slick({
+    autoplay: true,
+    accessibility: true,
+    draggable: true,
+    autoplaySpeed: 10000
+  });
+
+  if (new RegExp(/blog/).exec(this.location.pathname) != null) {
+    $('.navbar-nav a[href="/blog"').addClass('active');
+  } else {
+    $('.navbar-nav a[href="' + this.location.pathname + '"]')
+      .addClass('active');
+  }
+
+  if( $('.navbar-nav a.active').length == 0 ) {
+    $(".navbar-nav a.default-active")
+      .addClass('active');
+  }
+});
