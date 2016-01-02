bad5f2b5dc32a62cdc7275263db8e6619cdf0293
Jim OKelly <thatrubylove@gmail.com>
2016-01-02 17:38:17 -0600

    slide no more

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require bootstrap.min
 //= require smooth-scroll.min
-//= require slick.min
 //= require clickdesk.min
 //= require hotjar.min
 //= require ganalytics.min
@@ -11,13 +10,6 @@
 smoothScroll.init();
 
 $(document).ready(function() {
-
-  $('.slide').slick({
-    autoplay: true,
-    accessibility: true,
-    draggable: true,
-    autoplaySpeed: 10000
-  });
 
   if (new RegExp(/\/blog/).exec(this.location.pathname) != null) {
     $('.navbar-nav a[href="/blog"').addClass('active');
