25da98de45b5ed47f41f141d1de8a6b977a3b313
Jim OKelly <thatrubylove@gmail.com>
2016-01-05 17:56:05 -0600

    added meta news tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,19 @@ module ApplicationHelper
     custom || default
   end
 
+  def meta_keyword_tags
+    return "" unless content_for(:keyword_tags)
+    tags = content_for(:keyword_tags).split(",").map(&:strip)
+
+    article_tags = tags.map do |kt|
+      "<meta property=\"article:tag\" content=\"#{kt}\">"
+    end
+
+    news_keywords = "<meta name=\"news_keywords\" content=\"#{tags.join(", ")}\" />"
+
+    raw [news_keywords, article_tags].join("\n")
+  end
+
   def backend_url
     if Rails.env.development?
       "http://backend.devschool.dev:4000"
