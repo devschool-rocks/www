e7492dd9aa31661a0ac4ba3c17d8788904a58120
Jim OKelly <thatrubylove@gmail.com>
2016-01-05 20:54:36 -0600

    merged what and how

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
         via: :get, as: :coding_bootcamp_landing
 
   match "/coding-bootcamp"      => "web_page#what",   via: :get,  as: :what
-  match "/coding-bootcamp/how"  => "web_page#how",    via: :get,   as: :how
   match "/coding-bootcamp/where"=> "web_page#where",  via: :get,   as: :where
   match "/coding-bootcamp/who"  => "web_page#who",    via: :get,   as: :who
   match "/coding-bootcamp/price"=> "web_page#price",  via: :get,   as: :price
@@ -20,7 +19,7 @@ Rails.application.routes.draw do
   get "/sitemap", controller: :sitemap, action: :index
 
   match '/', :to => redirect('/coding-bootcamp'), via: :get
-  match '/how', :to => redirect('/coding-bootcamp/how'), via: :get
+  match '/how', :to => redirect('/coding-bootcamp'), via: :get
   match '/where', :to => redirect('/coding-bootcamp/where'), via: :get
   match '/who', :to => redirect('/coding-bootcamp/who'), via: :get
   match '/price', :to => redirect('/coding-bootcamp/price'), via: :get
