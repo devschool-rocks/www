fec69bed5bc1b899201c8d7a1adfd898bfb2579f
Jim OKelly <thatrubylove@gmail.com>
2016-01-23 14:34:47 -0600

    added coursereport grant

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,6 +1,7 @@
 class PromotionsController < ApplicationController
   def show
     @affiliate = Affiliate.find_by_code(params[:code])
+    redirect_to :root and return if @affiliate.nil?
     cookies.permanent['promo-code'] = @affiliate.code
   end
 end
