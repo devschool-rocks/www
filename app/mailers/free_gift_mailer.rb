b118e7ed9b35df36ad918570087c355273013744
Jim OKelly <thatrubylove@gmail.com>
2016-01-27 14:21:58 -0600

    deliver mails?

--- app/mailers/free_gift_mailer.rb
+++ app/mailers/free_gift_mailer.rb
@@ -1,8 +1,8 @@
 class FreeGiftMailer < ApplicationMailer
   default from: "do-not-reply@devschool.rocks"
 
-  def notify_jim(name, twiiter, email)
-    @name, @twitter, @email = first, twitter, email
+  def notify_jim(name, twitter, email)
+    @name, @twitter, @email = name, twitter, email
     mail(to:      "jim@devschool.rocks",
          subject: "#{name} (#{twitter}) wants a free preview session!")
   end
