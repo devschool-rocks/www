5bd8c049282c338851792fc98c3b62c6582fe8ea
Jim OKelly <thatrubylove@gmail.com>
2016-02-01 00:35:49 -0600

    new structure, less structure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,17 +10,3 @@
 
 smoothScroll.init();
 
-$(document).ready(function() {
-
-  if (new RegExp(/\/blog/).exec(this.location.pathname) != null) {
-    $('.navbar-nav a[href="/blog"').addClass('active');
-  } else {
-    $('.navbar-nav a[href="' + this.location.pathname + '"]')
-      .addClass('active');
-  }
-
-  if( $('.navbar-nav a.active').length == 0 ) {
-    $(".navbar-nav a.default-active")
-      .addClass('active');
-  }
-});
