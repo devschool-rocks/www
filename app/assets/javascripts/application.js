ae85e05f770439e52d620f70a8e4dbfbce63b079
Jim OKelly <thatrubylove@gmail.com>
2016-02-01 02:44:56 -0600

    added countdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,8 +5,29 @@
 //= require rate_it.min
 //= require formValidation.min
 //= require formValidationBootstrapFramework.min
+//= require countdown.min
 
 //= require_tree .
 
 smoothScroll.init();
 
+
+Date.prototype.getSecondMonday = function () {
+  var d = new Date (this.getFullYear(), this.getMonth(), 1, 0, 0, 0);
+  d.setDate(d.getDate() + 15 - d.getDay());
+  return d;
+}
+
+jQuery(document).ready(function() {
+  var d = new Date().getSecondMonday();
+  $('#countdown_dashboard').countDown({
+    targetDate: {
+      'day':   d.getDay()+1,
+      'month': d.getMonth()+1,
+      'year':  d.getYear()+1900,
+      'hour':  0,
+      'min':   0,
+      'sec':   0
+    }
+  });
+});
