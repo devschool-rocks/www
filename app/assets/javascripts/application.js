362225f8400df8fdd9fc59de06a159fb877385df
Jim OKelly <thatrubylove@gmail.com>
2016-02-01 14:06:45 -0600

    fixed timer?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,11 +2,11 @@
 //= require bootstrap.min
 //= require smooth-scroll.min
 //= require js.cookie
-//= require rate_it.min
 //= require formValidation.min
 //= require formValidationBootstrapFramework.min
 //= require countdown.min
 
+//= require_self
 //= require_tree .
 
 smoothScroll.init();
