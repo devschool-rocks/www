c7748a76c5a1ff708d70a752c8a5679ea6e1ca63
Jim OKelly <thatrubylove@gmail.com>
2016-02-02 17:16:48 -0600

    fixed applied image

--- app/controllers/free_gifts_controller.rb
+++ app/controllers/free_gifts_controller.rb
@@ -1,7 +1,6 @@
 class FreeGiftsController < ApplicationController
   def create
     p = [params[:name], params[:email]]
-    FreeGiftMailer.notify_jim(*p).deliver_now
     FreeGiftMailer.notify_previewer(*p).deliver_now
     redirect_to :gift_claimed
     cookies.permanent['free-session-claimed'] = 1
